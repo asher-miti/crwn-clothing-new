0178f6f7176956f3a00bb79143fbb067b5f6dfcc
asher-miti <asher_3@hotmail.co.uk>
2022-04-19 22:51:44 +0100

    styling sign-in/up page

--- src/routes/authentication/authentication.component.js
+++ src/routes/authentication/authentication.component.js
@@ -1,20 +1,11 @@
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
-import {
-  signInWithGooglePopup,
-  auth,
-  createUserDocumentFromAuth,
-} from '../../utils/firebase/firebase.utils';
+
+import './authentication.styles.scss';
 
 const Authentication = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
     </div>
