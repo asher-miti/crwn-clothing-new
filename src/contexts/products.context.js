7ca4a40caa9ce5992e239497197835870230c045
asher-miti <asher_3@hotmail.co.uk>
2022-05-02 17:43:20 +0100

    updates for products context

--- src/contexts/products.context.js
+++ src/contexts/products.context.js
@@ -2,17 +2,20 @@ import { createContext, useState, useEffect } from 'react';
 
 import PRODUCTS from '../shop-data.json';
 
-export const ProductContext = createContext({
+export const ProductsContext = createContext({
   products: [],
   setProducts: () => {},
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState(PRODUCTS);
+  console.log(products);
 
   const value = { products, setProducts };
 
   return (
-    <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
   );
 };
